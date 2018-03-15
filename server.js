312c03f1deaf6ae43f968f256da2815060ac41e0
David Samuelsen <david@pfg.dk>
2018-03-15 01:36:29 +0100

    fas

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const app = app = express();
+const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
