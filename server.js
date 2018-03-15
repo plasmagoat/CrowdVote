d83da45cb3c0f240b76c02c020870ac812732697
David Samuelsen <david@pfg.dk>
2018-03-15 01:42:32 +0100

    raghghm

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
+
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
 
 const port = process.env.PORT || 8000;
 
 app.use(express.static(__dirname + '/public'));
 
-server.listen(port, function () {
+
+app.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
+
+const io = require('socket.io')(app);
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
